cff7ee13c9e9334606158a87eff2c99fd647bfa6
hyc6446 <644691334@qq.com>
2019-03-26 17:32:29 +0800

    cookie池代码

--- proweb/proweb/run_tyc.py
+++
@@ -1,6 +0,0 @@
-from scrapy.cmdline import execute
-
-
-execute(['scrapy','crawlall'])
-
-#scrapy crawl tyc -s JOBDIR=job_main/001
--- qiyemulu/qiyemulu/run_shop.py
+++
@@ -1,8 +0,0 @@
-from scrapy.cmdline import execute
-# from subprocess import Popen
-# # subprocess.Popen("scrapy crawl comname")
-# execute(['scrapy','crawl','comname'])
-# execute(['scrapy','crawl','findname'])
-execute(['scrapy','crawl','baseinfo'])
-# execute(['scrapy','crawl','nianbao'])
-
